cebb280117cc55b27d76b5b9e7e01ed1f472595f
mateusz <mateusz_wojciechowski_98@wp.pl>
2020-07-19 11:29:11 +0200

    creating gui

--- gui/frames/top_menu/__init__.py
+++ gui/frames/top_menu/__init__.py
@@ -7,10 +7,15 @@ class TopMenuFrame(tk.Frame):
     def __init__(self, top):
         tk.Frame.__init__(self, top)
         self.configure(background=TMF_BG)
+
         self.fuel_list_btn = TopMenuButton(self)
-        self.fuel_list_btn.grid()
+        self.fuel_list_btn.pack(side="left")
         self.fuel_list_btn.configure(text='''Lista paliw''')
 
         self.survey_list_btn = TopMenuButton(self)
-        self.survey_list_btn.grid(row=0, column=1)
+        self.survey_list_btn.pack(side="left")
         self.survey_list_btn.configure(text='''Lista pomiarów''')
+
+        self.configure_btn = TopMenuButton(self)
+        self.configure_btn.pack(side="right")
+        self.configure_btn.configure(text='Konfiguracja')
