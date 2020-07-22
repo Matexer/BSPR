c352dd38c0a1963c607b7765ac8aca91deb32974
mateusz <mateusz_wojciechowski_98@wp.pl>
2020-07-22 21:04:23 +0200

    Changed container system

--- gui/frames/fuels_list.py
+++ gui/frames/fuels_list.py
@@ -7,8 +7,10 @@ class FuelsListFrame(ActionFrame):
     def __init__(self, top):
         ActionFrame.__init__(self, top)
 
-        btn_container, self.buttons = self.create_btn_container()
-        list_container, tree = self.create_list_container()
+        btn_container = tk.Frame(self)
+        list_container = tk.Frame(self)
+        self.buttons = self.create_btns(btn_container)
+        tree = self.create_list(list_container)
 
         self.buttons[0].configure(command=lambda: top.change_frame(1))
 
@@ -20,18 +22,18 @@ class FuelsListFrame(ActionFrame):
         tree.set_columns(("test 1 dddddddddddddddddddddkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkm", "test 2", "test 3"))
         tree.set_columns_width(tree_width, (0.6, 0.2, 0.2))
 
-    def create_btn_container(self):
-        btn_container = tk.Frame(self)
-        add_btn = AddButton(btn_container)
+    @staticmethod
+    def create_btns(top):
+        add_btn = AddButton(top)
         add_btn.pack(side="left")
-        edit_btn = EditButton(btn_container)
+        edit_btn = EditButton(top)
         edit_btn.pack(side="left", padx=10)
-        delete_btn = DeleteButton(btn_container)
+        delete_btn = DeleteButton(top)
         delete_btn.pack(side="left")
-        return btn_container, (add_btn, edit_btn, delete_btn)
+        return add_btn, edit_btn, delete_btn
 
-    def create_list_container(self):
-        list_container = tk.Frame(self)
-        tree = TreeList(list_container)
+    @staticmethod
+    def create_list(top):
+        tree = TreeList(top)
         tree.pack(fill="both", expand=1)
-        return list_container, tree
+        return tree
--- gui/frames/surveys_list.py
+++ gui/frames/surveys_list.py
@@ -4,15 +4,18 @@ from gui.elements import *
 from .fuels_list import FuelsListFrame
 
 
-class SurveysListFrame(ActionFrame):
+class SurveysListFrame(FuelsListFrame):
     def __init__(self, top):
         ActionFrame.__init__(self, top)
 
-        list_container, tree = self.create_list_container()
+        options_container = tk.Frame(self)
+        self.cboxs, self.buttons = self.create_options(options_container)
+        list_container = tk.Frame(self)
+        tree = self.create_list(list_container)
 
         self.buttons[0].configure(command=lambda: top.change_frame(3))
 
-        btn_container.pack(side="top", anchor="e", pady=5)
+        options_container.pack(side="top", fill="x", pady=5)
         list_container.pack(side="top", fill="both", expand=1)
 
         top.update()
@@ -20,33 +23,26 @@ class SurveysListFrame(ActionFrame):
         tree.set_columns(("test 1 dddddddddddddddddddddkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkm", "test 2", "test 3"))
         tree.set_columns_width(tree_width, (0.6, 0.2, 0.2))
 
-    def create_options_container(self):
-        options_container = tk.Frame(self)
-        cbox_container, cboxs = self.create_cbox_container()
-        btn_container, buttons = self.create_btn_container(options_container)
-
-    def create_cbox_container(self, top):
+    def create_options(self, top):
         cbox_container = tk.Frame(top)
-        fuel_label = tk.Label(cbox_container)
+        cboxs = self.create_cboxs(cbox_container)
+        buttons_container = tk.Frame(top)
+        buttons = self.create_btns(buttons_container)
+        cbox_container.pack(side="left")
+        buttons_container.pack(side="right")
+        return cboxs, buttons
+
+    @staticmethod
+    def create_cboxs(top):
+        fuel_label = tk.Label(top)
         fuel_label.configure(text="Paliwo")
-        fuel_cbox = ttk.Combobox(cbox_container)
-        type_label = tk.Label(cbox_container)
+        fuel_cbox = ttk.Combobox(top)
+        type_label = tk.Label(top)
         type_label.configure(text="Rodzaj pomiaru")
-        type_cbox = ttk.Combobox(cbox_container)
-        return cbox_container, (fuel_cbox, type_cbox)
+        type_cbox = ttk.Combobox(top)
 
-    def create_btn_container(self, top):
-        btn_container = tk.Frame(top)
-        add_btn = AddButton(btn_container)
-        add_btn.pack(side="left")
-        edit_btn = EditButton(btn_container)
-        edit_btn.pack(side="left", padx=10)
-        delete_btn = DeleteButton(btn_container)
-        delete_btn.pack(side="left")
-        return btn_container, (add_btn, edit_btn, delete_btn)
-
-    def create_list_container(self):
-        list_container = tk.Frame(self)
-        tree = TreeList(list_container)
-        tree.pack(fill="both", expand=1)
-        return list_container, tree
+        fuel_label.pack(side="left")
+        fuel_cbox.pack(side="left")
+        type_label.pack(side="left")
+        type_cbox.pack(side="left")
+        return fuel_cbox, type_cbox
