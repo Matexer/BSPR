8e7533708de9f9c89d7a0ad35a880deefda4d499
mateusz <mateusz_wojciechowski_98@wp.pl>
2020-07-22 15:58:46 +0200

    adding addfuel

--- gui/frames/add_fuel.py
+++ gui/frames/add_fuel.py
@@ -5,11 +5,21 @@ from gui.elements import InputTable
 class AddFuelFrame(tk.Frame):
     def __init__(self, top):
         tk.Frame.__init__(self, top)
-        variables = (("atttttttttttt", "b", "c"),
-                     ("e", "f", "h"))
+
+        table = self.create_inputs_container()
+        table.pack(side="top", anchor="w")
+
+    def create_inputs_container(self):
+        variables = (("Średnica zewnętrzna ładunku paliwa [mm]",
+                      "Średnica wewnętrzna ładunku paliwa [mm]",
+                      "Długość ładunku paliwa [mm]",
+                      "Masa paliwa [g]"),
+                     ("f", "h"))
         properties = {"ipadx": 0,
                       "ipady": 0,
                       "padx": 5,
                       "pady": 5,
                       "sticky": "W"}
         table = InputTable(self, variables)
+        return table
+
