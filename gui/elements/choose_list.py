070e6573cb490d12da1fb9cecbfc067c77ad30d0
mateusz <mateusz_wojciechowski_98@wp.pl>
2020-09-12 09:42:07 +0200

    added comment section to choose list

--- gui/elements/choose_list.py
+++ gui/elements/choose_list.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from .tree_list import TreeList
 from .plots import PlotFrame
 from .btns import Button
+from typing import Tuple, AnyStr
 
 
 class ChooseList(tk.Frame):
@@ -10,29 +11,31 @@ class ChooseList(tk.Frame):
     def __init__(self, top, *args, **kwargs):
         super().__init__(top, *args, **kwargs)
 
-        self.tree_frame, self.mark_all_btn = self.__create_tree_frame()
-        self.plot_frame, self.plot_buttons = self.__create_plot_frame()
+        self.tree_frame, self.mark_all_btn = self.__create_tree_frame(self)
+        self.plot_frame, self.plot_buttons = self.__create_plot_frame(self)
+        self.comment_frame, self.comment = self.__create_comment_frame(self)
 
-        self.tree_frame.pack(side="left", fill="both")
+        self.tree_frame.pack(side="left", fill="y")
         self.plot_frame.pack(side="right", fill="both")
+        self.comment_frame.pack(side="bottom", fill="x")
 
-        self.plots_data = []
+        self.plots_data = Tuple[Tuple, int, AnyStr]
         self.drawn_plots = []
 
-    def __create_tree_frame(self):
+    def __create_tree_frame(self, top):
         CheckTreeList = TreeList
         CheckTreeList.CHECK_OPTION = True
-        tree_frame = CheckTreeList(self)
-        tree_frame.tree.bind("<<TreeviewSelect>>", lambda e: self.__draw_plots())
+        tree_frame = CheckTreeList(top)
+        tree_frame.tree.bind("<<TreeviewSelect>>", lambda e: self.__show_surveys())
         mark_all_btn = Button(tree_frame, text="Wybierz wszystkie",
                               command=lambda: tree_frame.toggle_all())
         mark_all_btn.pack(side="bottom", anchor="w")
         return tree_frame, mark_all_btn
 
-    def __create_plot_frame(self):
+    def __create_plot_frame(self, top):
         ExtendedPlotFrame = PlotFrame
         ExtendedPlotFrame.plot_fig_size = self.PLOT_FIG_SIZE
-        plot_frame = ExtendedPlotFrame(self)
+        plot_frame = ExtendedPlotFrame(top)
         plot_frame.hide_toolbar()
         plot_frame.canvas.get_tk_widget().pack_configure(padx=0)
         btn_container = tk.Frame(plot_frame)
@@ -44,24 +47,45 @@ class ChooseList(tk.Frame):
         btn_container.pack(side="bottom", fill="both", padx=10)
         return plot_frame, (edit_btn, set_t_btn)
 
-    def __draw_plots(self):
+    @staticmethod
+    def __create_comment_frame(top):
+        container = tk.Frame(top)
+        title = tk.Label(container, text="KOMENTARZ: ")
+        content = tk.Label(container)
+        title.pack(anchor="w")
+        content.pack(anchor="w")
+        container.pack(fill="x", padx=10)
+        return container, content
+
+    def __show_surveys(self):
+        if not self.plots_data:
+            raise AttributeError("Lack of plot's data")
+
+        plot = self.plot_frame.plot
+        selected_items = self.tree_frame.tree.selection()
+
+        ids = [self.tree_frame.tree.index(item) for item in selected_items]
+
+        if not selected_items:
+            return
+
+        self.__clean_plots()
+        self.__draw_plots(plot, ids)
+        self.__show_comment(self.plots_data[ids[0]][2])
+
+    def __clean_plots(self):
         for plot in self.drawn_plots:
             plot.remove()
         self.drawn_plots = []
 
-        if not self.plots_data:
-            raise AttributeError("Lack of data to plot")
-
-        plot = self.plot_frame.plot
-        selected_items = self.tree_frame.tree.selection()
-        if not selected_items:
-            return
-
-        for item in selected_items:
-            index = self.tree_frame.tree.index(item)
+    def __draw_plots(self, plot, ids):
+        for index in ids:
             data = self.plots_data[index]
             y_data = data[0]
-            x_data = [data[1] * i for i in range(len(data) + 1)]
+            x_data = [data[1] * i for i in range(len(y_data))]
             self.drawn_plots.append(*plot.plot(x_data, y_data))
 
         self.plot_frame.canvas.draw()
+
+    def __show_comment(self, text):
+        self.comment.configure(text=text)
--- gui/frames/templates/config_calculation_frame.py
+++ gui/frames/templates/config_calculation_frame.py
@@ -72,7 +72,7 @@ class ConfigCalculationFrameTemplate(ScrolledFrameTemplate):
             self.create_surveys_container(self.interior, columns)
 
         self.surveys_list.tree_frame.set_data(((1, 2), (3, 4)))
-        self.surveys_list.plots_data = [((1, 2, 4), 5), ((4, 5, 4), 2)]
+        self.surveys_list.plots_data = [((1, 2, 4), 5, "egg"), ((4, 5, 4), 2, "milk")]
 
         cboxes = ({
                  "Metoda całkowania": ("liniowa", "trapezów", "Simpsona")},
